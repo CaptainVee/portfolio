c6ab0442bf1067b579ebf9ef79686e82aaaaab9f
unknown <captainvee3@gmail.com>
2020-11-10 19:34:29 -0800

    something jor

--- django_project/settings.py
+++ django_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['captainvee.herokuapp.com']
+ALLOWED_HOSTS = ['captainvee2.herokuapp.com']
 
 
 # Application definition
