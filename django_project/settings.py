ffdc706b03c4d7be767d48c0a821a2e82a7dfc5d
CaptainVee <captainvee3@gmail.com>
2023-03-14 18:20:15 +0100

    feat: changed debug

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'dfdf'#os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.captainvee.com', 'web-production-34b7.up.railway.app', 'localhost', 'captainvee.com']
 
